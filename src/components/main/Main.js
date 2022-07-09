4a6c44b1b94f3c1516b25567246139bba8c2791c
lt96mr <eeswar.krishnavarjula@ing.com>
2022-07-09 10:58:48 +0200

    update new changes

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Main.css";
 import axios from 'axios';
+import { Columns, Column } from 'react-flex-columns'
 
 export default function Main(props) {
     let obj ={};
@@ -11,6 +12,7 @@ export default function Main(props) {
     const [financialData, setFinancialData] = useState([]);
     const [financialQuote, setFinancialQuote] = useState([]);
     const [cashflowStatement, setCashflowStatement] = useState([]);
+    const [assetProfile, setAssetProfile] = useState([]);
     const [loading, setLoading] = useState(true);
     const keyParams = ["Margin", "Asset turnover", "Leverage", "Return on equity"]
     const overViewText = [
@@ -32,65 +34,28 @@ export default function Main(props) {
       if(statement){
         return {
           method: 'GET',
-          url: `https://yfapi.net/v11/finance/quoteSummary/${symbolName+'.NS'}?lang=en&region=IN&modules=${statement}`,
+          url: `https://yfapi.net/v11/finance/quoteSummary/${symbolName+'.NS'}?lang=en&region=IN&modules=${'financialData'},${'incomeStatementHistory'},${'balanceSheetHistory'},${'cashflowStatementHistory'},${'assetProfile'}`,
           params: {symbol: symbolName, region: 'IN'},
           headers: headers
       }
     }
-
       return {
         method: 'GET',
         url: `https://yfapi.net/v6/finance/quote?region=IN&lang=en&symbols=${symbolName+'.NS'}`,
         params: {symbol: symbolName, region: 'IN'},
         headers: headers
       }};
-
-      const fetchIncomeData = async () =>{
-        setLoading(true);
-        try {
-          const {data: response} = await axios.request(options('incomeStatementHistory'));
-          console.log("fetchIncomeData",  response);
-          setIncomeStatement(response?.quoteSummary?.result[0]?.incomeStatementHistory?.incomeStatementHistory);
-        } catch (error) {
-          console.error(error.response.data.message);
-          setErrorMesssage(error.response.data.message);
-        }
-        setLoading(false);
-      }
       
-
-      const fetchBalanceSheetData = async () =>{
-        setLoading(true);
-        try {
-          const {data: response} = await axios.request(options('balanceSheetHistory'));
-          console.log("fetchBalanceSheetData", response);
-          setBalanceSheet(response?.quoteSummary?.result[0]?.balanceSheetHistory?.balanceSheetStatements);
-        } catch (error) {
-          console.error(error.message);
-          setErrorMesssage(error.response.data.message);
-        }
-        setLoading(false);
-      }
-
       const fetchFinancialData = async () =>{
         setLoading(true);
         try {
-          const {data: response} = await axios.request(options('financialData'));
+          const {data: response} = await axios.request(options(true));
           console.log("fetchFinancialData", response);
           setFinancialData(response?.quoteSummary?.result[0]?.financialData);
-        } catch (error) {
-          console.error(error.message);
-          setErrorMesssage(error.response.data.message);
-        }
-        setLoading(false);
-      }
-
-      const fetchCashflowData = async () =>{
-        setLoading(true);
-        try {
-          const {data: response} = await axios.request(options('cashflowStatementHistory'));
-          console.log("fetchCashflowData", response);
+          setBalanceSheet(response?.quoteSummary?.result[0]?.balanceSheetHistory?.balanceSheetStatements);
+          setIncomeStatement(response?.quoteSummary?.result[0]?.incomeStatementHistory?.incomeStatementHistory);
           setCashflowStatement(response?.quoteSummary?.result[0]?.cashflowStatementHistory?.cashflowStatements);
+          setAssetProfile(response?.quoteSummary?.result[0]?.assetProfile);
         } catch (error) {
           console.error(error.message);
           setErrorMesssage(error.response.data.message);
@@ -113,10 +78,7 @@ export default function Main(props) {
 
     function handleSubmit(e) {
         e.preventDefault();
-        fetchIncomeData();
-        fetchBalanceSheetData();
         fetchFinancialData();
-        fetchCashflowData();
         fetchFinancialQuote();
     }
         return (
@@ -154,148 +116,176 @@ export default function Main(props) {
                   <button type="submit">Submit</button>
               </form>
 
-          {incomeStatement?.length > 0 && financialData && <div><table>
-          <thead>
-            <tr>
-              <th></th>
-              {
-                  incomeStatement?.map((value,key) => {
-                      return (
-                      <th key={key}>{new Date(value?.endDate?.fmt).getFullYear()}</th>
-                      )
-                  })
-              }
-              <th>Comments</th>
-            </tr>
-          </thead>
-              <tr>
-                  <th>Margin</th>
+          {incomeStatement?.length > 0 && financialData && <div>
+            <div className="summary">
+            <h2 className="compdesc">
+              Company Description:
+            </h2>
+            <p className="para">{assetProfile?.longBusinessSummary}</p>
+            </div>
+            <div className="summary">
+            <Columns gutters>
+        <Column flex>
+          <h2>Sector:</h2>
+        </Column>
+        <Column flex className="sector-name">
+          <h4>{assetProfile?.sector}</h4>
+        </Column>
+        <Column flex>
+          <h4></h4>
+        </Column>
+        <Column flex>
+          <h4></h4>
+        </Column>
+      </Columns>
+            </div>
+{/* 
+            <Columns gutterSize={2}>
+              <Column flex>Sector</Column>
+              <Column flex >{assetProfile?.sector}</Column>
+            </Columns> */}      
+            <table>
+              <thead>
+                <tr>
+                  <th></th>
                   {
-                      incomeStatement?.map((value, key) => {
+                      incomeStatement?.map((value,key) => {
                           return (
-                              <td>{((value?.netIncome?.raw / value?.totalRevenue?.raw) * 100).toFixed(2)}%</td>
+                          <th key={key}>{new Date(value?.endDate?.fmt).getFullYear()}</th>
                           )
                       })
                   }
-                  <td>{overViewText[0]}</td>
-              </tr>
-
-              <tr>
-                  <th>Asset turnover</th>
-                  {
-                      incomeStatement?.map((value, key) => {
-                        if(incomeStatement[key+1]){
-                          return (<td>{((value?.totalRevenue?.raw / ((balanceSheet[key]?.totalAssets?.raw+balanceSheet[key+1]?.totalAssets?.raw)/2))).toFixed(2)}</td>)
+                  <th>Comments</th>
+                </tr>
+              </thead>
+                  <tr>
+                      <th>Margin</th>
+                      {
+                          incomeStatement?.map((value, key) => {
+                              return (
+                                  <td>{((value?.netIncome?.raw / value?.totalRevenue?.raw) * 100).toFixed(2)}%</td>
+                              )
+                          })
                       }
-                          else return (
-                              <td>{((value?.totalRevenue?.raw / balanceSheet[key]?.totalAssets?.raw)).toFixed(2)}</td>
-                          )
-                      })
-                  }
-                  <td>{overViewText[1]}</td>
-              </tr>
+                      <td>{overViewText[0]}</td>
+                  </tr>
 
-              <tr>
-                  <th>Financial Leverage</th>
-                  {
-                      incomeStatement?.map((value, key) => {
-                        // let val = ((((balanceSheet[key]?.totalAssets?.raw+ balanceSheet[key+1]?.totalAssets?.raw)/2) / ((balanceSheet[key]?.totalStockholderEquity?.raw+balanceSheet[key+1]?.totalStockholderEquity?.raw)/2))).toFixed(2);
-                        // if(val==='NaN'){
-                        //   return (<td>-</td>)
-                        // }
-                        if(incomeStatement[key+1]){
-                          return (<td>{((((balanceSheet[key]?.totalAssets?.raw+ balanceSheet[key+1]?.totalAssets?.raw)/2) / ((balanceSheet[key]?.totalStockholderEquity?.raw+balanceSheet[key+1]?.totalStockholderEquity?.raw)/2))).toFixed(2)}</td>)
-                      }
-                          else return (
-                              <td>{((balanceSheet[key]?.totalAssets?.raw / balanceSheet[key]?.totalStockholderEquity?.raw)).toFixed(2)}</td>
-                          )
-                      })
-                  }
-                  <td>{overViewText[2]}</td>
-              </tr>
-
-              <tr>
-                  <th>ROE</th>
-                  {
-                      incomeStatement?.map((value, key) => {
-                        // let val = ((value?.netIncome?.raw / ((balanceSheet[key]?.totalStockholderEquity?.raw + balanceSheet[key+1]?.totalStockholderEquity?.raw)/2)) * 100).toFixed(2);
-                        // if(val==='NaN'){
-                        //   return (<td>-</td>)
-                        // }
-                          if(incomeStatement[key+1]){
-                              return (<td>{((value?.netIncome?.raw / ((balanceSheet[key]?.totalStockholderEquity?.raw + balanceSheet[key+1]?.totalStockholderEquity?.raw)/2)) * 100).toFixed(2)} %</td>) 
+                  <tr>
+                      <th>Asset turnover</th>
+                      {
+                          incomeStatement?.map((value, key) => {
+                            if(incomeStatement[key+1]){
+                              return (<td>{((value?.totalRevenue?.raw / ((balanceSheet[key]?.totalAssets?.raw+balanceSheet[key+1]?.totalAssets?.raw)/2))).toFixed(2)}</td>)
                           }
-                          else return (
-                              <td>{((value?.netIncome?.raw / (balanceSheet[key]?.totalStockholderEquity?.raw + 0)/2) * 100).toFixed(2)}%</td>
-                          )
-                      })
-                  }
-                  <td>{overViewText[3]}</td>
-              </tr>
+                              else return (
+                                  <td>{((value?.totalRevenue?.raw / balanceSheet[key]?.totalAssets?.raw)).toFixed(2)}</td>
+                              )
+                          })
+                      }
+                      <td>{overViewText[1]}</td>
+                  </tr>
 
-              <tr>
-                  <th>ROCE</th>
-                  {
-                      incomeStatement?.map((value, key) => {
-                        let val = ((value?.ebit?.raw/(balanceSheet[key]?.totalStockholderEquity?.raw + balanceSheet[key]?.longTermDebt?.raw))*100).toFixed(2);
-                        if(val==='NaN'){
-                          return (<td>-</td>)
-                        }
-                          else return (
-                              <td>{((value?.ebit?.raw/(balanceSheet[key]?.totalStockholderEquity?.raw + balanceSheet[key]?.longTermDebt?.raw))*100).toFixed(2)}%</td>
-                              // <td>{((value?.ebit?.raw/(balanceSheet[key]?.totalAssets?.raw - balanceSheet[key]?.totalCurrentLiabilities?.raw))*100).toFixed(2)}%</td>
-                          )
-                      })
-                  }
-                  <td>{overViewText[4]}</td>
-              </tr>
+                  <tr>
+                      <th>Financial Leverage</th>
+                      {
+                          incomeStatement?.map((value, key) => {
+                            // let val = ((((balanceSheet[key]?.totalAssets?.raw+ balanceSheet[key+1]?.totalAssets?.raw)/2) / ((balanceSheet[key]?.totalStockholderEquity?.raw+balanceSheet[key+1]?.totalStockholderEquity?.raw)/2))).toFixed(2);
+                            // if(val==='NaN'){
+                            //   return (<td>-</td>)
+                            // }
+                            if(incomeStatement[key+1]){
+                              return (<td>{((((balanceSheet[key]?.totalAssets?.raw+ balanceSheet[key+1]?.totalAssets?.raw)/2) / ((balanceSheet[key]?.totalStockholderEquity?.raw+balanceSheet[key+1]?.totalStockholderEquity?.raw)/2))).toFixed(2)}</td>)
+                          }
+                              else return (
+                                  <td>{((balanceSheet[key]?.totalAssets?.raw / balanceSheet[key]?.totalStockholderEquity?.raw)).toFixed(2)}</td>
+                              )
+                          })
+                      }
+                      <td>{overViewText[2]}</td>
+                  </tr>
 
-              <tr>
-                  <th>EBITDA margin</th>
-                  {
-                      incomeStatement?.map((value, key) => {
-                        let val = (((( value?.ebit?.raw + cashflowStatement[key]?.depreciation?.raw) / value?.totalRevenue?.raw)) * 100).toFixed(2);
-                        if(val==='NaN'){
-                          return (<td>-</td>)
-                        }
-                          return (
-                              <td>{(((( value?.ebit?.raw + cashflowStatement[key]?.depreciation?.raw) / value?.totalRevenue?.raw)) * 100).toFixed(2)}%</td>
-                          )
-                      })
-                  }
-                  <td>{overViewText[5]}</td>
-              </tr>
+                  <tr>
+                      <th>ROE</th>
+                      {
+                          incomeStatement?.map((value, key) => {
+                            // let val = ((value?.netIncome?.raw / ((balanceSheet[key]?.totalStockholderEquity?.raw + balanceSheet[key+1]?.totalStockholderEquity?.raw)/2)) * 100).toFixed(2);
+                            // if(val==='NaN'){
+                            //   return (<td>-</td>)
+                            // }
+                              if(incomeStatement[key+1]){
+                                  return (<td>{((value?.netIncome?.raw / ((balanceSheet[key]?.totalStockholderEquity?.raw + balanceSheet[key+1]?.totalStockholderEquity?.raw)/2)) * 100).toFixed(2)} %</td>) 
+                              }
+                              else return (
+                                  <td>{((value?.netIncome?.raw / (balanceSheet[key]?.totalStockholderEquity?.raw + 0)/2) * 100).toFixed(2)}%</td>
+                              )
+                          })
+                      }
+                      <td>{overViewText[3]}</td>
+                  </tr>
 
-              <tr>
-                  <th>PE Ratio</th>
-                  {/* {
-                      incomeStatement?.map((value, key) => {
-                          return ( */}
-                              <td>{(financialQuote?.regularMarketPrice / financialQuote?.epsTrailingTwelveMonths).toFixed(2)}</td>
-                              <td>-</td>
-                              <td>-</td>
-                              <td>-</td>
-                          {/* )
-                      })
-                  } */}
-                  <td>{overViewText[6]}</td>
-              </tr>
+                  <tr>
+                      <th>ROCE</th>
+                      {
+                          incomeStatement?.map((value, key) => {
+                            let val = ((value?.ebit?.raw/(balanceSheet[key]?.totalStockholderEquity?.raw + balanceSheet[key]?.longTermDebt?.raw))*100).toFixed(2);
+                            if(val==='NaN'){
+                              return (<td>-</td>)
+                            }
+                              else return (
+                                  <td>{((value?.ebit?.raw/(balanceSheet[key]?.totalStockholderEquity?.raw + balanceSheet[key]?.longTermDebt?.raw))*100).toFixed(2)}%</td>
+                                  // <td>{((value?.ebit?.raw/(balanceSheet[key]?.totalAssets?.raw - balanceSheet[key]?.totalCurrentLiabilities?.raw))*100).toFixed(2)}%</td>
+                              )
+                          })
+                      }
+                      <td>{overViewText[4]}</td>
+                  </tr>
 
-              <tr>
-                  <th>P/BV Ratio</th>
-                  {/* {
-                      incomeStatement?.map((value, key) => {
-                          return ( */}
-                              <td>{(financialQuote?.regularMarketPrice / financialQuote?.bookValue).toFixed(2)}</td>
-                              <td>-</td>
-                              <td>-</td>
-                              <td>-</td>
-                          {/* )
-                      })
-                  } */}
-                  <td>{overViewText[7]}</td>
-              </tr>
-        </table>
+                  <tr>
+                      <th>EBITDA margin</th>
+                      {
+                          incomeStatement?.map((value, key) => {
+                            let val = (((( value?.ebit?.raw + cashflowStatement[key]?.depreciation?.raw) / value?.totalRevenue?.raw)) * 100).toFixed(2);
+                            if(val==='NaN'){
+                              return (<td>-</td>)
+                            }
+                              return (
+                                  <td>{(((( value?.ebit?.raw + cashflowStatement[key]?.depreciation?.raw) / value?.totalRevenue?.raw)) * 100).toFixed(2)}%</td>
+                              )
+                          })
+                      }
+                      <td>{overViewText[5]}</td>
+                  </tr>
+
+                  <tr>
+                      <th>PE Ratio</th>
+                      {/* {
+                          incomeStatement?.map((value, key) => {
+                              return ( */}
+                                  <td>{(financialQuote?.regularMarketPrice / financialQuote?.epsTrailingTwelveMonths).toFixed(2)}</td>
+                                  <td>-</td>
+                                  <td>-</td>
+                                  <td>-</td>
+                              {/* )
+                          })
+                      } */}
+                      <td>{overViewText[6]}</td>
+                  </tr>
+
+                  <tr>
+                      <th>P/BV Ratio</th>
+                      {/* {
+                          incomeStatement?.map((value, key) => {
+                              return ( */}
+                                  <td>{(financialQuote?.regularMarketPrice / financialQuote?.bookValue).toFixed(2)}</td>
+                                  <td>-</td>
+                                  <td>-</td>
+                                  <td>-</td>
+                              {/* )
+                          })
+                      } */}
+                      <td>{overViewText[7]}</td>
+                  </tr>
+            </table>
         <span>*ROE - Return on equity</span><br/>
         <span>*ROCE - Return on Capital Employed</span><br/>
         <span>*EBITDA - Earnings Before Interest, Taxes, Depreciation, and Amortization</span><br/>
